c09e0f7c6da76739627a404bfa00a9ca06f8ba39
RawF4sh <david.fridshtand@gmail.com>
2025-08-05 11:33:13 -0700

    testing

--- visualizeList.py
+++ visualizeList.py
@@ -1,4 +1,4 @@
-from toDoList import toDoList
+#from toDoList import toDoList
 import matplotlib.pyplot as plt
 
 class visualizeList:
@@ -6,9 +6,19 @@ class visualizeList:
         self.arrToDoList = arrToDoList
 
 
-    def piChartVisualize(self, listNum):
-        labels = 'Not Completed', 'Completed'
-        sizes = [len(self.arrToDoList[listNum])-self.arrToDoList[listNum].how_many_complete, self.arrToDoList[listNum].how_many_complete]
+    def pieChartVisualize(self, listNum):
+        #labels = 'Not Completed', 'Completed'
+        #sizes = [len(self.arrToDoList[listNum])-self.arrToDoList[listNum].how_many_complete, self.arrToDoList[listNum].how_many_complete]
+
+       #fig, ax = plt.subplots()
+        #ax.pie(sizes, labels=labels)
+        labels = 'Complete', 'Incomplete'
+        sizes = [5,15]
 
         fig, ax = plt.subplots()
         ax.pie(sizes, labels=labels)
+
+
+a = visualizeList("f")
+
+a.pieChartVisualize(5)
