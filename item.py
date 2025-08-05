df336d68012a57a173b330038b85d39f9fe0f74e
Ianluckaimia <rkyamamoto@twc.com>
2025-08-05 15:59:42 -0700

    .time() added

--- item.py
+++ item.py
@@ -1,24 +1,26 @@
 from tensorflow.python.ops.metrics_impl import false_negatives
-from datetime import datetime
-
+import datetime
 
 class Item:
     def __init__(self, item_name, time_due):
         self.item_name = item_name
         # self.time_created=time_created
         self.time_due = time_due
-        self.time_finished = 0
+        self.time_finished = None
         self.completed = False
         self.on_time = False
 
     def complete_item(self):
         if (input("Did you finish this task, yes or no? ") == "yes"):
             self.completed = True
-            self.time_finished = datetime.now()
+            self.time_finished = datetime.datetime.now().time()
 
     def late_work_check(self):
-        if self.time_due >= self.time_finished:
-            self.on_time = True
+        if self.time_finished:
+            if self.time_due >= self.time_finished:
+                self.on_time = True
+        else:
+            print("Task not completed")
 
     def __str__(self):
         return ("Task Name: " + self.item_name + "Due Date: " + self.time_due + "Completion Status: " + (
