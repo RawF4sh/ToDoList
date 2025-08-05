b09f09c500e0d0cbee1e3b2b717a0fb1d86fed15
AustintheCoder123 <austiny7@icloud.com>
2025-08-05 12:12:04 -0700

    Added a singular comment above the __str__ function :P

--- toDoList.py
+++ toDoList.py
@@ -8,12 +8,13 @@ class ToDoList:
     #Function to check if an item (a task) is complete
     def CheckComplete(self):
         for item in self.items:
-            if item.complete: #change when I have access to
+            if item.complete:
                 self.how_many_completed+=1
 
     #Function to add an item (task) to the list of items (the whole to-do list)
     def AddItem(self, Item):
-        self.items.append(Item) #Does this work? Appending the whole class?
+        self.items.append(Item)
 
+    #Cool lil thing so that it prints when an instance of the class is printed
     def __str__(self):
         return f"There are {len(self.items)} tasks on the to-do list. {self.how_many_completed} of those tasks has been completed."
