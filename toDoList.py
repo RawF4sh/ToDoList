d05dc27270ad3077fa7b24bdcadd6c9c5966cefc
AustintheCoder123 <austiny7@icloud.com>
2025-08-05 11:24:30 -0700

    Added CheckComplete function

--- toDoList.py
+++ toDoList.py
@@ -1,5 +1,11 @@
+# from item import Item
+
 class ToDoList:
     def __init__(self):
         self.items = []
         self.how_many_completed = 0
-    def
+
+    def CheckComplete(self):
+        for item in self.items:
+            if item.complete: #change when I have access to
+                self.how_many_completed+=1
