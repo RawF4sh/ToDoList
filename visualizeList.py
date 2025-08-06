bf01d3ed92a38fa05c6cac1cab106982c0baaa90
RawF4sh <david.fridshtand@gmail.com>
2025-08-06 09:18:00 -0700

    Added Comments

--- visualizeList.py
+++ visualizeList.py
@@ -19,18 +19,26 @@ class visualizeList:
     def pieChartVisualize(self):
         fig, ax = plt.subplots(figsize=(10, 5))
 
-
+        #Labels for pi chart and legend
         labels = ['Not Completed', 'Completed']
         currentList = self.toDoListItem
 
-
         #items = [self.incompleteNames, self.completedNames]
+
+        #How many not complete and complete items
         sizes = [len(currentList.items), currentList.how_many_completed]
 
-        wedges, texts, autotexts = ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90) #
+        #Creates the pie charts with the pie chart labels and percentages
+        wedges, texts, autotexts = ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+
+        #Creates a legend
         ax.legend(wedges, labels,
                   title="Items",
                   loc="right",
                   bbox_to_anchor=(1, 0, 0.6, 1))
+
+        #Title
         ax.set_title("Today's To Do List")
+
+        #Shows the Window
         plt.show()
