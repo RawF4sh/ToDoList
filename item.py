8ee4b44dc80f32d709704339376c5c1fab529b71
Ianluckaimia <rkyamamoto@twc.com>
2025-08-06 09:22:30 -0700

    comments for items

--- item.py
+++ item.py
@@ -4,17 +4,18 @@ import datetime
 class Item:
     def __init__(self, item_name, time_due):
         self.item_name = item_name
-        # self.time_created=time_created
         self.time_due = time_due
         self.time_finished = None
         self.completed = False
         self.on_time = False
 
+    #checks a task as finished
     def complete_item(self):
         if (input("Did you finish this task, yes or no? ") == "yes"):
             self.completed = True
             self.time_finished = datetime.datetime.now().time()
 
+    #checks if a task is done late
     def late_work_check(self):
         if self.time_finished:
             if self.time_due >= self.time_finished:
@@ -22,6 +23,7 @@ class Item:
         else:
             print("Task not completed")
 
+    #sets a string that represents an item
     def __str__(self):
         return ("Task Name: " + self.item_name + "Due Date: " + self.time_due + "Completion Status: " + (
             "Complete" if self.completed else "Incomplete"))
